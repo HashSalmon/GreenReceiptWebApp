9915a3bc66b66950fa1b8b912073bd582a3d6fd0
jordanwanlass <kingsleyzheng0716@gmail.com>
2015-01-14 14:13:50 -0700

    added in comments and generated javadoc

--- src/main/java/Utilities/GreenReceiptUtil.java
+++ src/main/java/Utilities/GreenReceiptUtil.java
@@ -10,8 +10,18 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
+/**
+ * Holds the main utilities for the application.
+ *
+ * @author Jordan Wanlass
+ */
 public class GreenReceiptUtil {
 
+    /**
+     * Returns all of the users receipts that the user has in the system.
+     *
+     * @return a list of all of the users receipts
+     */
     public static List<Receipt> getReceipts() {
         RestTemplate restTemplate = new RestTemplate();
         UserInfo userInfo = (UserInfo) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -34,13 +44,19 @@ public class GreenReceiptUtil {
         return receipts;
     }
 
-    public static Receipt getReceipt(String recieptId) {
+    /**
+     * Retrieves a specific receipt
+     *
+     * @param receiptId This is the id for the receipt that corresponds to the database.
+     * @return          Returns a receipt object that corresponds to the id passed in.
+     */
+    public static Receipt getReceipt(String receiptId) {
         RestTemplate restTemplate = new RestTemplate();
         UserInfo userInfo = (UserInfo) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         Gson gson = new Gson();
-        String apiCall = "https://greenreceipt.net/api/Receipt?id=" + recieptId;
+        String apiCall = "https://greenreceipt.net/api/Receipt?id=" + receiptId;
 
         headers.set("Authorization", "Bearer " + userInfo.getAccess_token());
         ResponseEntity responseEntity = null;
